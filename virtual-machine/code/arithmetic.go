f322dad2ae6976239943644c865b2e0c572f9f43
Dersu Ozan <dersuozan3@gmail.com>
2022-02-06 17:38:16 +0300

    Refactoring arithmetic file

--- virtual-machine/code/arithmetic.go
+++ virtual-machine/code/arithmetic.go
@@ -5,149 +5,132 @@ import (
 	"strings"
 )
 
-var currentBranchNum = 0
-var ArithmeticCommandsAndWriters = map[string]func() string {
+var ArithmeticCommandsAndWriters = map[string]interface{} {
 	"add": translateAdd,
 	"sub": translateSub,
 	"neg": translateNeg,
-	"eq": translateWithBranch(translateEq),
-	"gt": translateWithBranch(translateGt),
-	"lt": translateWithBranch(translateLt),
+	"eq": translateEq,
+	"gt": translateGt,
+	"lt": translateLt,
 	"and": translateAnd,
 	"or": translateOr,
 	"not": translateNot,
 }
 
-// Closure for eq, gt and lt commands. Increments branch number
-func translateWithBranch(handler func() string) func() string {
-	return func() string {
-		currentBranchNum++
-		return handler()
-	}
+
+func translateAdd(builder strings.Builder) strings.Builder {
+	builder.WriteString("// Add\n")
+	builder.WriteString("@SP\n")
+	builder.WriteString("M=M-1\n")
+	builder.WriteString("A=M\n")
+	builder.WriteString("D=M\n")
+	builder.WriteString("A=A-1\n")
+	builder.WriteString("M=D+M\n")
+	return builder
 }
 
-func translateAdd() string {
-	var asmLine strings.Builder
-	asmLine.WriteString("// Add\n")
-	asmLine.WriteString("@SP\n")
-	asmLine.WriteString("M=M-1\n")
-	asmLine.WriteString("A=M\n")
-	asmLine.WriteString("D=M\n")
-	asmLine.WriteString("A=A-1\n")
-	asmLine.WriteString("M=D+M\n")
-	return asmLine.String()
+func translateSub(builder strings.Builder) strings.Builder {
+	builder.WriteString("// Sub\n")
+	builder.WriteString("@SP\n")
+	builder.WriteString("M=M-1\n")
+	builder.WriteString("A=M\n")
+	builder.WriteString("D=M\n")
+	builder.WriteString("A=A-1\n")
+	builder.WriteString("M=M-D\n")
+	return builder
 }
 
-func translateSub() string {
-	var asmLine strings.Builder
-	asmLine.WriteString("// Sub\n")
-	asmLine.WriteString("@SP\n")
-	asmLine.WriteString("M=M-1\n")
-	asmLine.WriteString("A=M\n")
-	asmLine.WriteString("D=M\n")
-	asmLine.WriteString("A=A-1\n")
-	asmLine.WriteString("M=M-D\n")
-	return asmLine.String()
+func translateNeg(builder strings.Builder) strings.Builder {
+	builder.WriteString("// Neg\n")
+	builder.WriteString("@SP\n")
+	builder.WriteString("A=M-1\n")
+	builder.WriteString("M=-M\n")
+	return builder
 }
 
-func translateNeg() string {
-	var asmLine strings.Builder
-	asmLine.WriteString("// Neg\n")
-	asmLine.WriteString("@SP\n")
-	asmLine.WriteString("A=M-1\n")
-	asmLine.WriteString("M=-M\n")
-	return asmLine.String()
+func translateEq(builder strings.Builder, branchNum int) strings.Builder {
+	builder.WriteString("// Eq\n")
+	builder.WriteString("@SP\n")
+	builder.WriteString("M=M-1\n")
+	builder.WriteString("A=M\n")
+	builder.WriteString("D=M\n")
+	builder.WriteString("A=A-1\n")
+	builder.WriteString("D=M-D\n")
+	builder.WriteString("M=1\n")
+	builder.WriteString(fmt.Sprintf("@ELSE%d\n", branchNum))
+	builder.WriteString("D;JNE\n")
+	builder.WriteString(fmt.Sprintf("@CONTINUE%d\n", branchNum))
+	builder.WriteString("0;JMP\n")
+	builder.WriteString(fmt.Sprintf("(ELSE%d)\n", branchNum))
+	builder.WriteString("M=0\n")
+	builder.WriteString(fmt.Sprintf("(CONTINUE%d)\n", branchNum))
+	return builder
 }
 
-func translateEq() string {
-	var asmLine strings.Builder
-	asmLine.WriteString("// Eq\n")
-	asmLine.WriteString("@SP\n")
-	asmLine.WriteString("M=M-1\n")
-	asmLine.WriteString("A=M\n")
-	asmLine.WriteString("D=M\n")
-	asmLine.WriteString("A=A-1\n")
-	asmLine.WriteString("D=M-D\n")
-	asmLine.WriteString("M=1\n")
-	asmLine.WriteString(fmt.Sprintf("@ELSE%d\n", currentBranchNum))
-	asmLine.WriteString("D;JNE\n")
-	asmLine.WriteString(fmt.Sprintf("@CONTINUE%d\n", currentBranchNum))
-	asmLine.WriteString("0;JMP\n")
-	asmLine.WriteString(fmt.Sprintf("(ELSE%d)\n", currentBranchNum))
-	asmLine.WriteString("M=0\n")
-	asmLine.WriteString(fmt.Sprintf("(CONTINUE%d)\n", currentBranchNum))
-	return asmLine.String()
+func translateGt(builder strings.Builder, branchNum int) strings.Builder {
+	builder.WriteString("// Gt\n")
+	builder.WriteString("@SP\n")
+	builder.WriteString("M=M-1\n")
+	builder.WriteString("A=M\n")
+	builder.WriteString("D=M\n")
+	builder.WriteString("A=A-1\n")
+	builder.WriteString("D=M-D\n")
+	builder.WriteString("M=1\n")
+	builder.WriteString(fmt.Sprintf("@ELSE%d\n", branchNum))
+	builder.WriteString("D;JLE\n")
+	builder.WriteString(fmt.Sprintf("@CONTINUE%d\n", branchNum))
+	builder.WriteString("0;JMP\n")
+	builder.WriteString(fmt.Sprintf("(ELSE%d)\n", branchNum))
+	builder.WriteString("M=0\n")
+	builder.WriteString(fmt.Sprintf("(CONTINUE%d)\n", branchNum))
+	return builder
 }
 
-func translateGt() string {
-	var asmLine strings.Builder
-	asmLine.WriteString("// Gt\n")
-	asmLine.WriteString("@SP\n")
-	asmLine.WriteString("M=M-1\n")
-	asmLine.WriteString("A=M\n")
-	asmLine.WriteString("D=M\n")
-	asmLine.WriteString("A=A-1\n")
-	asmLine.WriteString("D=M-D\n")
-	asmLine.WriteString("M=1\n")
-	asmLine.WriteString(fmt.Sprintf("@ELSE%d\n", currentBranchNum))
-	asmLine.WriteString("D;JLE\n")
-	asmLine.WriteString(fmt.Sprintf("@CONTINUE%d\n", currentBranchNum))
-	asmLine.WriteString("0;JMP\n")
-	asmLine.WriteString(fmt.Sprintf("(ELSE%d)\n", currentBranchNum))
-	asmLine.WriteString("M=0\n")
-	asmLine.WriteString(fmt.Sprintf("(CONTINUE%d)\n", currentBranchNum))
-	return asmLine.String()
+func translateLt(builder strings.Builder, branchNum int) strings.Builder {
+	builder.WriteString("// Lt\n")
+	builder.WriteString("@SP\n")
+	builder.WriteString("M=M-1\n")
+	builder.WriteString("A=M\n")
+	builder.WriteString("D=M\n")
+	builder.WriteString("A=A-1\n")
+	builder.WriteString("D=M-D\n")
+	builder.WriteString("M=1\n")
+	builder.WriteString(fmt.Sprintf("@ELSE%d\n", branchNum))
+	builder.WriteString("D;JGE\n")
+	builder.WriteString(fmt.Sprintf("@CONTINUE%d\n", branchNum))
+	builder.WriteString("0;JMP\n")
+	builder.WriteString(fmt.Sprintf("(ELSE%d)\n", branchNum))
+	builder.WriteString("M=0\n")
+	builder.WriteString(fmt.Sprintf("(CONTINUE%d)\n", branchNum))
+	return builder
 }
 
-func translateLt() string {
-	var asmLine strings.Builder
-	asmLine.WriteString("// Lt\n")
-	asmLine.WriteString("@SP\n")
-	asmLine.WriteString("M=M-1\n")
-	asmLine.WriteString("A=M\n")
-	asmLine.WriteString("D=M\n")
-	asmLine.WriteString("A=A-1\n")
-	asmLine.WriteString("D=M-D\n")
-	asmLine.WriteString("M=1\n")
-	asmLine.WriteString(fmt.Sprintf("@ELSE%d\n", currentBranchNum))
-	asmLine.WriteString("D;JGE\n")
-	asmLine.WriteString(fmt.Sprintf("@CONTINUE%d\n", currentBranchNum))
-	asmLine.WriteString("0;JMP\n")
-	asmLine.WriteString(fmt.Sprintf("(ELSE%d)\n", currentBranchNum))
-	asmLine.WriteString("M=0\n")
-	asmLine.WriteString(fmt.Sprintf("(CONTINUE%d)\n", currentBranchNum))
-	return asmLine.String()
+func translateAnd(builder strings.Builder) strings.Builder {
+	builder.WriteString("// And\n")
+	builder.WriteString("@SP\n")
+	builder.WriteString("M=M-1\n")
+	builder.WriteString("A=M\n")
+	builder.WriteString("D=M\n")
+	builder.WriteString("A=A-1\n")
+	builder.WriteString("M=D&M\n")
+	return builder
 }
 
-func translateAnd() string {
-	var asmLine strings.Builder
-	asmLine.WriteString("// And\n")
-	asmLine.WriteString("@SP\n")
-	asmLine.WriteString("M=M-1\n")
-	asmLine.WriteString("A=M\n")
-	asmLine.WriteString("D=M\n")
-	asmLine.WriteString("A=A-1\n")
-	asmLine.WriteString("M=D&M\n")
-	return asmLine.String()
+func translateOr(builder strings.Builder) strings.Builder {
+	builder.WriteString("// Or\n")
+	builder.WriteString("@SP\n")
+	builder.WriteString("M=M-1\n")
+	builder.WriteString("A=M\n")
+	builder.WriteString("D=M\n")
+	builder.WriteString("A=A-1\n")
+	builder.WriteString("M=D|M\n")
+	return builder
 }
 
-func translateOr() string {
-	var asmLine strings.Builder
-	asmLine.WriteString("// Or\n")
-	asmLine.WriteString("@SP\n")
-	asmLine.WriteString("M=M-1\n")
-	asmLine.WriteString("A=M\n")
-	asmLine.WriteString("D=M\n")
-	asmLine.WriteString("A=A-1\n")
-	asmLine.WriteString("M=D|M\n")
-	return asmLine.String()
-}
-
-func translateNot() string {
-	var asmLine strings.Builder
-	asmLine.WriteString("// Not\n")
-	asmLine.WriteString("@SP\n")
-	asmLine.WriteString("A=M-1\n")
-	asmLine.WriteString("M=!M\n")
-	return asmLine.String()
+func translateNot(builder strings.Builder) strings.Builder {
+	builder.WriteString("// Not\n")
+	builder.WriteString("@SP\n")
+	builder.WriteString("A=M-1\n")
+	builder.WriteString("M=!M\n")
+	return builder
 }
