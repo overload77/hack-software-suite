6ae6e3f0c0c87d0c49f28f27d7273583828143b1
Dersu Ozan <dersuozan3@gmail.com>
2022-02-13 00:28:44 +0300

    Refactoring memory segment translator

--- virtual-machine/code/memory.go
+++ virtual-machine/code/memory.go
@@ -1,7 +1,10 @@
 package code
 
-import "fmt"
-import "strings"
+import (
+	"fmt"
+	"log"
+	"strings"
+)
 
 type MemorySegmentTranslator struct {
 	Handlers map[string]func(string, string)
@@ -10,7 +13,7 @@ type MemorySegmentTranslator struct {
 }
 
 func GetMemorySegmentTranslator(builder *strings.Builder) *MemorySegmentTranslator {
-	memSegCommand := &MemorySegmentTranslator {
+	memoryTranslator := &MemorySegmentTranslator {
 		builder: builder,
 		mappings: map[string]string {
 			"local": "LCL",
@@ -19,48 +22,141 @@ func GetMemorySegmentTranslator(builder *strings.Builder) *MemorySegmentTranslat
 			"that": "THAT",
 		},
 	}
-	memSegCommand.Handlers = map[string]func(string, string) {
-		"push": memSegCommand.translatePush,
-		"pop": memSegCommand.translatePop,
+	memoryTranslator.Handlers = map[string]func(string, string) {
+		"push": memoryTranslator.translatePush,
+		"pop": memoryTranslator.translatePop,
 	}
 
-	return memSegCommand
+	return memoryTranslator
 }
 
-func (memSegCommand *MemorySegmentTranslator) translatePush(segment string, index string) {
-	segmentSymbol := memSegCommand.mappings[segment]
-	memSegCommand.builder.WriteString(fmt.Sprintf("// Push %s %s\n", segment, index))
-	memSegCommand.builder.WriteString(fmt.Sprintf("@%s\n", segmentSymbol))
-	memSegCommand.builder.WriteString("D=M\n")
-	memSegCommand.builder.WriteString(fmt.Sprintf("@%s\n", index))
-	memSegCommand.builder.WriteString("A=D+A\n")
-	memSegCommand.builder.WriteString("D=M\n")
-	memSegCommand.builder.WriteString("@SP\n")
-	memSegCommand.builder.WriteString("M=M+1\n")
-	memSegCommand.builder.WriteString("A=M-1\n")
-	memSegCommand.builder.WriteString("M=D\n")
+func (memoryTranslator *MemorySegmentTranslator) translatePush(segment, index string) {
+	memoryTranslator.builder.WriteString(fmt.Sprintf("// Push %s %s\n", segment, index))
+	memoryTranslator.writeSegmentValueToDReg(segment, index)
+	memoryTranslator.pushDRegisterToStack()
 }
 
-func (memSegCommand *MemorySegmentTranslator) translatePop(segment string, index string) {
-	segmentSymbol := memSegCommand.mappings[segment]
-	memSegCommand.builder.WriteString(fmt.Sprintf("// Pop %s %s\n", segment, index))
-	memSegCommand.builder.WriteString("@SP\n")
-	memSegCommand.builder.WriteString("M=M-1\n")
-	memSegCommand.builder.WriteString("A=M\n")
-	memSegCommand.builder.WriteString("D=M\n")
-	memSegCommand.builder.WriteString("@13\n")
-	memSegCommand.builder.WriteString("M=D\n")
+func (memoryTranslator *MemorySegmentTranslator) translatePop(segment, index string) {
+	memoryTranslator.builder.WriteString(fmt.Sprintf("// Pop %s %s\n", segment, index))
+	memoryTranslator.popStackToDReg()
+	memoryTranslator.pushDRegisterToSegment(segment, index)
+}
 
-	memSegCommand.builder.WriteString(fmt.Sprintf("@%s\n", segmentSymbol))
-	memSegCommand.builder.WriteString("D=M\n")
-	memSegCommand.builder.WriteString(fmt.Sprintf("@%s\n", index))
-	memSegCommand.builder.WriteString("D=D+A\n")
-	memSegCommand.builder.WriteString("@14\n")
-	memSegCommand.builder.WriteString("M=D\n")
+/// Lower level methods for Push translator
 
-	memSegCommand.builder.WriteString("@13\n")
-	memSegCommand.builder.WriteString("D=M\n")
-	memSegCommand.builder.WriteString("@14\n")
-	memSegCommand.builder.WriteString("A=M\n")
-	memSegCommand.builder.WriteString("M=D\n")
+// Get's segments value and puts it into D register
+func (memoryTranslator *MemorySegmentTranslator) writeSegmentValueToDReg(segment, index string) {
+	switch segment {
+	case "constant":
+		memoryTranslator.writeConstantToDReg(index)
+	case "pointer":
+		memoryTranslator.writePointerToDReg(index)
+	default:
+		memoryTranslator.writeMappedSegmentsToDReg(segment, index)
+	}
+}
+
+// Writes first 4 segments(LCL, ARG, THIS and THAT) value to the D register
+func (memoryTranslator *MemorySegmentTranslator) writeMappedSegmentsToDReg(segment, index string) {
+	segmentSymbol, ok := memoryTranslator.mappings[segment]
+	if !ok {
+		log.Fatal("Invalid memory segment")
+	}
+	memoryTranslator.builder.WriteString(fmt.Sprintf("@%s\n", segmentSymbol))
+	memoryTranslator.builder.WriteString("D=M\n")
+	memoryTranslator.builder.WriteString(fmt.Sprintf("@%s\n", index))
+	memoryTranslator.builder.WriteString("A=D+A\n")
+	memoryTranslator.builder.WriteString("D=M\n")
+}
+
+// Writes pointer segment to D register
+func (memoryTranslator *MemorySegmentTranslator) writePointerToDReg(index string) {
+	if index == "0" {
+		memoryTranslator.builder.WriteString("@THIS\n")
+	} else if index == "1" {
+		memoryTranslator.builder.WriteString("@THAT\n")
+	} else {
+		log.Fatal("Invalid pointer segment index")
+	}
+	memoryTranslator.builder.WriteString("D=M\n")
+}
+
+// Writes constant segment to D register
+func (memoryTranslator *MemorySegmentTranslator) writeConstantToDReg(index string) {
+	memoryTranslator.builder.WriteString(fmt.Sprintf("@%s\n", index))
+	memoryTranslator.builder.WriteString("D=A\n")
+}
+
+// Pushes D register to stack
+func (memoryTranslator *MemorySegmentTranslator) pushDRegisterToStack() {
+	memoryTranslator.builder.WriteString("@SP\n")
+	memoryTranslator.builder.WriteString("M=M+1\n")
+	memoryTranslator.builder.WriteString("A=M-1\n")
+	memoryTranslator.builder.WriteString("M=D\n")
+}
+
+/// Lower level methods for Pop translator
+
+// Pops the stack and puts value in D register
+func (memoryTranslator *MemorySegmentTranslator) popStackToDReg() {
+	memoryTranslator.builder.WriteString("@SP\n")
+	memoryTranslator.builder.WriteString("M=M-1\n")
+	memoryTranslator.builder.WriteString("A=M\n")
+	memoryTranslator.builder.WriteString("D=M\n")
+}
+
+func (memoryTranslator *MemorySegmentTranslator) pushDRegisterToSegment(segment, index string) {
+	switch segment {
+	case "pointer":
+		memoryTranslator.writeDRegToPointer(index)
+	default:
+		memoryTranslator.writeDRegToMappedSegments(segment, index)
+	}
+}
+
+func (memoryTranslator *MemorySegmentTranslator) writeDRegToMappedSegments(segment, index string) {
+	memoryTranslator.writeDRegToR13()
+	memoryTranslator.writeSegmentPointerToR14(segment, index)
+	memoryTranslator.writeR13ToSegmentStoredOnR14()
+}
+
+func (memoryTranslator *MemorySegmentTranslator) writeDRegToPointer(index string) {
+	if index == "0" {
+		memoryTranslator.builder.WriteString("@THIS\n")
+	} else if index == "1" {
+		memoryTranslator.builder.WriteString("@THAT\n")
+	} else {
+		log.Fatal("Invalid pointer segment index")
+	}
+	memoryTranslator.builder.WriteString("M=D\n")
+}
+
+// Puts value in D register to register 13
+func (memoryTranslator *MemorySegmentTranslator) writeDRegToR13() {
+	memoryTranslator.builder.WriteString("@13\n")
+	memoryTranslator.builder.WriteString("M=D\n")
+}
+
+// Put pointer to the segment to register 14
+func (memoryTranslator *MemorySegmentTranslator) writeSegmentPointerToR14(segment, index string) {
+	segmentSymbol, ok := memoryTranslator.mappings[segment]
+	if !ok {
+		log.Fatal("Invalid memory segment")
+	}
+	memoryTranslator.builder.WriteString(fmt.Sprintf("@%s\n", segmentSymbol))
+	memoryTranslator.builder.WriteString("D=M\n")
+	memoryTranslator.builder.WriteString(fmt.Sprintf("@%s\n", index))
+	memoryTranslator.builder.WriteString("D=D+A\n")
+	memoryTranslator.builder.WriteString("@14\n")
+	memoryTranslator.builder.WriteString("M=D\n")
+}
+
+// Put value in register 13(Popped value from the stack) to the segment
+// which it's pointer is on register 14
+func (memoryTranslator *MemorySegmentTranslator) writeR13ToSegmentStoredOnR14() {
+	memoryTranslator.builder.WriteString("@13\n")
+	memoryTranslator.builder.WriteString("D=M\n")
+	memoryTranslator.builder.WriteString("@14\n")
+	memoryTranslator.builder.WriteString("A=M\n")
+	memoryTranslator.builder.WriteString("M=D\n")
 }
