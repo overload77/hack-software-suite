29137b5ed274c915c477fb13c1933855c723d28e
Dersu Ozan <dersuozan3@gmail.com>
2022-03-07 11:22:53 +0300

    Extracting method

--- virtual-machine/code/parser.go
+++ virtual-machine/code/parser.go
@@ -18,19 +18,26 @@ var arithmeticCommands = map[string]struct{} {
 	"not": struct{}{},
 }
 
-// Parses instruction into it's type and operands
+// Parses instruction into it's type and operands and set's up context
 func (context *CodeContext) parseLine(line string) {
 	_, isArithmetic := arithmeticCommands[line]
 	if isArithmetic {
-		context.currentTranslator = context.arithmeticTranslator
-		context.currentCommand = line
+		context.setTranslatorAndArgs(context.arithmeticTranslator, line)
 	} else if strings.Contains(line, "push") || strings.Contains(line, "pop") {
 		split := strings.Split(line, " ")
-		context.currentTranslator = context.memorySegmentTranslator
-		context.currentCommand = split[0]
-		context.currentFirstArg = split[1]
-		context.currentSecondArg = split[2]
+		context.setTranslatorAndArgs(context.memorySegmentTranslator, split[0], split[1], split[2])
 	} else {
 		log.Fatalln("This should be handled")
+	}
+}
+
+// Sets the code context's current translator, command and optionally first and second args
+func (context *CodeContext) setTranslatorAndArgs(translator Translator, command string,
+												 args ...string) {
+	context.currentTranslator = translator
+	context.currentCommand = command
+	if len(args) != 0 {
+		context.currentFirstArg = args[0]
+		context.currentSecondArg = args[1]
 	}
 }
