0744dc4746a47d80ad87cc93e30f04c2d1ffcda1
Dersu Ozan <dersuozan3@gmail.com>
2022-03-08 22:13:20 +0300

    Adding branching package

---
+++ virtual-machine/code/branch/branch.go
@@ -0,0 +1,61 @@
+package branch
+
+import (
+	"fmt"
+	"log"
+	"strings"
+)
+
+type BranchTranslator struct {
+	Handlers map[string]func(string)
+	builder *strings.Builder
+	vmFileName string
+	currentFunction string
+}
+
+func GetBranchTranslator(builder *strings.Builder, vmFileName string) *BranchTranslator {
+	branchTranslator := &BranchTranslator {
+		builder: builder,
+		vmFileName: vmFileName,
+		currentFunction: "main",
+	}
+	branchTranslator.Handlers = map[string]func(string) {
+		"label": branchTranslator.translateLabel,
+		"goto": branchTranslator.translateGoto,
+		"if-goto": branchTranslator.translateIfGoto,
+	}
+	
+	return branchTranslator
+}
+
+func (translator *BranchTranslator) Translate(command, label, dummy string) {
+	if handlerMethod, isOk := translator.Handlers[command]; isOk {
+		handlerMethod(label)
+	} else {
+		log.Fatalln("Invalid branching command")
+	}
+}
+
+func (translator *BranchTranslator) translateLabel(label string) {
+	label = fmt.Sprintf("(%s.%s$%s)\n", translator.vmFileName,
+						translator.currentFunction, label)
+	translator.builder.WriteString(label)
+}
+
+func (translator *BranchTranslator) translateGoto(label string) {
+	jumpLocation := fmt.Sprintf("@%s.%s$%s\n", translator.vmFileName,
+								translator.currentFunction, label)
+	translator.builder.WriteString(jumpLocation)
+	translator.builder.WriteString("0;JMP\n")
+}
+
+func (translator *BranchTranslator) translateIfGoto(label string) {
+	jumpLocation := fmt.Sprintf("@%s.%s$%s\n", translator.vmFileName,
+								translator.currentFunction, label)
+	translator.builder.WriteString("@SP\n")
+	translator.builder.WriteString("M=M-1\n")
+	translator.builder.WriteString("A=M\n")
+	translator.builder.WriteString("D=M\n")
+	translator.builder.WriteString(jumpLocation)
+	translator.builder.WriteString("D;JNE\n")
+}
